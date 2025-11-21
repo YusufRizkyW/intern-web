b41ebf2c0044827491165a513c92124ae40d2123
YusufRizkyW <yusufrizkyawan1810@gmail.com>
2025-11-21 22:20:08 +0700

    update password text

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'reset' => 'Sandai telah direset!',
+    'sent' => 'Email link reset passwordmu terkirim, cek semua email atau bagian spam',
+    'throttled' => 'Tunggu beberapa saat sebelum mencoba lagi.',
+    'token' => 'Password reset token tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+];
